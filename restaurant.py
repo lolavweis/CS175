f849a6984b8287365b0ecfbc635f8877cc498890
lolavweis <114018347+lolavweis@users.noreply.github.com>
2023-10-01 17:44:18 -0400

    Restaurant

---
+++ restaurant.py
@@ -0,0 +1,33 @@
+#Lola Weis
+#CS-175
+#Resaurant
+
+vegetarian= str(input("Is anyone in your party vegetarian? "))
+vegan= str(input("Is anyone in your party vegan? "))
+gluten= str(input("Is anyone in your party gluten-free? "))
+
+if vegetarian==Yes:
+    if vegan==Yes:
+        if gluten== Yes:
+            print("The Chef's Kitchen"/n "Corner Cafe")
+            else:
+                print("The Chef's Kitchen"/n "Corner Cafe")
+        else:
+            if gluten== Yes:
+            print("Main Street Pizza Company"/n "The Chef's Kitchen"/n "Corner Cafe")
+            else:
+                print("Main Street Pizza Company"/n "The Chef's Kitchen"/n "Corner Cafe"/n "Mama's Fine Italian")
+
+else:
+    if vegan==Yes:
+        if gluten== Yes:
+            print("The Chef's Kitchen"/n "Corner Cafe")
+            else:
+                print("The Chef's Kitchen"/n "Corner Cafe")
+        else:
+             if gluten== Yes:
+            print("Main Street Pizza Company"/n "The Chef's Kitchen"/n "Corner Cafe")
+            else:
+                print("Main Street Pizza Company"/n "The Chef's Kitchen"/n "Corner Cafe"/n "Mama's Fine Italian"/n "Joe's Gourmet Burgers")
+                
+
