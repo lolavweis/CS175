47ef5516f7d823c7052714c06d64e48c0fe0836f
lolavweis <114018347+lolavweis@users.noreply.github.com>
2023-10-02 17:06:57 -0400

    restaurant.py

--- restaurant.py
+++ restaurant.py
@@ -6,28 +6,28 @@ vegetarian= str(input("Is anyone in your party vegetarian? "))
 vegan= str(input("Is anyone in your party vegan? "))
 gluten= str(input("Is anyone in your party gluten-free? "))
 
-if vegetarian==Yes:
-    if vegan==Yes:
-        if gluten== Yes:
-            print("The Chef's Kitchen"/n "Corner Cafe")
-            else:
-                print("The Chef's Kitchen"/n "Corner Cafe")
+if vegetarian=="Yes" or "yes":
+    if vegan=="Yes" or "yes":
+        if gluten=="Yes":
+            print("The Chef's Kitchen\n" "Corner Cafe")
         else:
-            if gluten== Yes:
-            print("Main Street Pizza Company"/n "The Chef's Kitchen"/n "Corner Cafe")
-            else:
-                print("Main Street Pizza Company"/n "The Chef's Kitchen"/n "Corner Cafe"/n "Mama's Fine Italian")
+            print("The Chef's Kitchen\n" "Corner Cafe")
+    else:
+        if gluten=="Yes" or "yes":
+            print("Main Street Pizza Company\n" "The Chef's Kitchen\n" "Corner Cafe")
+        else:
+            print("Main Street Pizza Company\n" "The Chef's Kitchen\n" "Corner Cafe\n" "Mama's Fine Italian")
 
 else:
-    if vegan==Yes:
-        if gluten== Yes:
-            print("The Chef's Kitchen"/n "Corner Cafe")
-            else:
-                print("The Chef's Kitchen"/n "Corner Cafe")
+    if vegan=="Yes" or "yes":
+        if gluten=="Yes" or "yes":
+            print("The Chef's Kitchen\n" "Corner Cafe")
         else:
-             if gluten== Yes:
-            print("Main Street Pizza Company"/n "The Chef's Kitchen"/n "Corner Cafe")
-            else:
-                print("Main Street Pizza Company"/n "The Chef's Kitchen"/n "Corner Cafe"/n "Mama's Fine Italian"/n "Joe's Gourmet Burgers")
+            print("The Chef's Kitchen\n" "Corner Cafe")
+    else:
+        if gluten=="Yes" or "yes":
+            print("Main Street Pizza Company\n" "The Chef's Kitchen\n" "Corner Cafe")
+        else:
+            print("Main Street Pizza Company\n" "The Chef's Kitchen\n" "Corner Cafe\n" "Mama's Fine Italian\n" "Joe's Gourmet Burgers")
                 
 
