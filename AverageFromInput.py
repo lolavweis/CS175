0e3d53cf0f8b1ccd530637054abb1835ae9cef39
lolavweis <114018347+lolavweis@users.noreply.github.com>
2023-11-08 22:55:29 -0500

    Update AverageFromInput.py

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -7,17 +7,23 @@ def main():
     totalsum=0
     numofnum=0
     txtfile= open('numbers.txt','r')
-    for linenum in txtfile:
-            num = float(linenum)
-            numofnum += 1
-            totalsum += num
-            print(f"I read in {numofnum} number(s) Current number is: {num:.2f} Total is: {totalsum:.2f}")
-    if numofnum > 0:
-        avg = (totalsum/numofnum)
-        print(f"Average: {avg:.1f}")
-    else:
-        print("Please check data that is in file.")
-    txtfile.close()
+    try:
+        for linenum in txtfile:
+            try:
+                num = float(linenum)
+                numofnum += 1
+                totalsum += num
+                print(f"I read in {numofnum} number(s) Current number is: {num:.2f}Total is: {totalsum:.2f}")
+            except ValueError:
+                print("Error converting to number.")
+        if numofnum > 0:
+            avg = (totalsum/numofnum)
+            print(f"Average: {avg:.1f}")
+        else:
+            print("Please check data that is in file.")
+        txtfile.close()
+    except IOError:
+        print("Error Reading File.")
 if __name__ == '__main__':
     main()
 
